6beada0788db947b962da0d90a6ddb25d5b25517
100eyes <besim.arnautovic@gmail.com>
2018-10-24 09:44:10 +0200

    Commiting changes to first_task.R script

--- First month/first_task.R
+++ First month/first_task.R
@@ -1,3 +1,6 @@
 # checking dummy values
-sapply(business, function(x) sum(is.na(x)))
-sapply(business, function(x) sum(as.integer(x == "")))
+na_num = sapply(yelp_business, function(x) sum(is.na(x)))
+total_nas = sum(na_num)
+dummy_percent = total_nas/(nrow(yelp_business)*ncol(yelp_business))*100
+which.max(na_num)
+which.min(na_num)
