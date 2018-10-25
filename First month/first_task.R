a05fbd1a2f026a6cae85c961e05b2c03858b5eca
100eyes <besim.arnautovic@gmail.com>
2018-10-26 00:52:07 +0200

    Separated yelp_business database into 3 databases by countries (USA and Canada) and added frequency for important attributes and checked for duplicates in business_id and name attributes in database.

--- First month/first_task.R
+++ First month/first_task.R
@@ -10,6 +10,12 @@ dummy_percent = total_nas/(nrow(yelp_business)*ncol(yelp_business))*100
 which.max(na_num)
 which.min(na_num)
 
+# Percentage of completeness by attributes
+percent_by_column <- na_num[1:length(na_num)]/nrow(yelp_business)*100
+which.max(percent_by_column)
+which.min(percent_by_column)
+important_attributes = percent_by_column[c("name", "address", "city", "state", "longitude", "latitude")]
+
 # Dummy values
 states <- table(yelp_business$state)
 sum(states[c("01", "10", "11", "4", "45", "6")])
@@ -31,3 +37,24 @@ yelp_business$latitude[which.min(yelp_business$latitude)]
 # Data frequency
 hist(yelp_business$stars, xlab = "Stars", main = "Stars frequency")
 hist(yelp_business$review_count, breaks = 1000, xlim = c(0, 150), ylim = c(0, 105000), xlab = "Review count", main = "Review count frequency")
+
+tail(sort(table(yelp_business$city)), n=10)
+tail(sort(table(yelp_business$state)))
+tail(sort(table(yelp_business$name)), n=10)
+
+
+# Sorting businesses by countries in North America
+us_business <- yelp_business[which(yelp_business$state %in% us_states$Abbreviation),]
+canada_business <- yelp_business[which(yelp_business$state %in% canada_states$Code),] 
+
+dummy_states <- yelp_business[which(!((yelp_business$state %in% us_states$Abbreviation) | (yelp_business$state %in% canada_states$Code))),]
+
+table(dummy_states$state)
+
+# Checking for duplicates in businessID and names
+businessID_occur <- data.frame(table(yelp_business$business_id))
+nrow(yelp_business[yelp_business$business_id %in% businessID_occur$Var1[businessID_occur$Freq > 1],])
+
+names_occur <- data.frame(table(yelp_business$name))
+same_names <- yelp_business[yelp_business$name %in% names_occur$Var1[names_occur$Freq >1],]
+nrow(same_names)
