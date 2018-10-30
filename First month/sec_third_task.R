583362588135500b57a0c954988f943f1438c060
100eyes <besim.arnautovic@gmail.com>
2018-10-30 22:17:33 +0100

    Working on task 2 and 3

--- First month/sec_third_task.R
+++ First month/sec_third_task.R
@@ -9,6 +9,11 @@ business_same_address <- yelp_business[yelp_business$address %in% same_address$V
 agg <- aggregate(geo_coordinates~address, business_same_address, length)
 agg1 <- aggregate(address~geo_coordinates, business_same_address, length)
 agg2 <- aggregate(cbind(latitude, longitude)~address, business_same_address, mean)
+agg2 <- agg2[order(agg2$latitude),]
 new_df <- data.frame(addres = agg$address, Consistent = agg$geo_coordinates == 1)
 agg <- agg[order(agg$latitude), ]
 agg1 <- agg1[order(agg)]
+
+agg3 <- aggregate(cbind(latitude, longitude)~address, business_same_address, max)
+agg4 <- aggregate(cbind(latitude, longitude)~address, business_same_address, min)
+agg3 <- agg3[order(agg3$latitude), ]
